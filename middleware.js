5f503b8c2a33b6b3856ffb713ac7a9e460495bab
Captain-fintech <y.akayev@gmail.com>
2025-11-12 22:08:18 +0400

    Add middleware for redirecting all requests

---
+++ middleware.js
@@ -0,0 +1,12 @@
+// middleware.js (корень репозитория)
+import { NextResponse } from 'next/server';
+
+export const config = {
+  matcher: '/:path*', // перехватываем все пути
+};
+
+export function middleware(req) {
+  const { pathname, search, hash } = req.nextUrl;
+  const dst = 'https://baysulu-onefile-rtdl.vercel.app' + pathname + search + hash;
+  return NextResponse.redirect(dst, 308);
+}
